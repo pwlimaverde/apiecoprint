d81018efaf7df1c446b2b729322343afddd5a892
PWLIMAVERDE <vorfast@gmail.com>
2019-09-22 15:40:18 -0300

    ajuste do admin em pcp_registro_op

--- pcp_registro_op/admin.py
+++ pcp_registro_op/admin.py
@@ -2,4 +2,11 @@ from django.contrib import admin
 from .models import RegistroOp
 
 
-admin.site.register(RegistroOp)
+class RegistroOpAdmin(admin.ModelAdmin):
+    fields = (('op', 'orcamento', 'quant'), ('cliente', 'vendedor'), ('servico'), ('entrada'), ('prev_entrega'))
+    list_display = ('op', 'quant', 'cliente', 'vendedor', 'servico', 'entrada', 'prev_entrega')
+    list_filter = ('vendedor', 'cliente')
+    search_fields = ('op', 'orcamento', 'cliente', 'vendedor', 'servico')
+
+
+admin.site.register(RegistroOp, RegistroOpAdmin)
