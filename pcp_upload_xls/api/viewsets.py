b41cb8123381c6fa27cc0a902b65393367069feb
PWLIMAVERDE <vorfast@gmail.com>
2019-09-22 22:03:57 -0300

    edição do create do pcp_upload_xls

--- pcp_upload_xls/api/viewsets.py
+++ pcp_upload_xls/api/viewsets.py
@@ -1,12 +1,18 @@
+from datetime import datetime
+import xlrd
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import (
     IsAuthenticated, IsAdminUser, DjangoModelPermissions,
     IsAuthenticatedOrReadOnly, DjangoModelPermissionsOrAnonReadOnly
 )
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from pcp_upload_xls.models import Upload_list_op
+from pcp_registro_op.models import RegistroOp
+from pcp_registro_entrega.models import RegistroEntrega
 from .serializers import UploadListOpSerializer
 
 
@@ -19,3 +25,49 @@ class UploadListOpViewSet(ModelViewSet):
     #filterset_fields = ('cliente', 'servico', 'quant')
     #filter_backends = (SearchFilter, )
     #search_fields = ['id', '=op__orcamento', '=op__op', '=op__quant', 'op__cliente', 'op__servico', '=op__vendedor', ]
+
+
+    def create(self, request, *args, **kwargs):
+
+        context = {}
+
+        xls_file = request.data['arquivo']
+
+        if not xls_file.name.endswith('.xls'):
+            return Response({'erro': 'o arquivo não é um xls'})
+
+        Upload_list_op.objects.update_or_create(descricao=xls_file.name, arquivo=xls_file)
+        caminho = 'static/arquivosxls/' + xls_file.name
+
+        workbook = xlrd.open_workbook(caminho)
+        worksheet = workbook.sheet_by_index(0)
+        datemode = workbook.datemode
+
+        listaxls = []
+
+        for row_num in range(worksheet.nrows):
+            row = worksheet.row_values(row_num)
+            row[5] = datetime(*xlrd.xldate_as_tuple(row[5], datemode))
+            row[8] = datetime(*xlrd.xldate_as_tuple(row[8], datemode))
+            listaxls.append(row)
+
+        for item in listaxls:
+            RegistroOp.objects.update_or_create(
+                orcamento=item[0],
+                cliente=item[1],
+                servico=item[2],
+                quant=item[3],
+                valor=item[4],
+                entrada=item[5],
+                vendedor=item[6],
+                op=item[7],
+                prev_entrega=item[8],
+            )
+
+            cons = RegistroOp.objects.all().order_by("-id")[0]
+            RegistroEntrega.objects.update_or_create(op=cons)
+
+        now = datetime.now().strftime("%d-%m-%y as %H:%M:%S")
+        context['listaxls'] = listaxls
+
+        return Response({'Status': 'Upload realizado com sucesso em: ' + str(now), 'Relação dos Upload:': context['listaxls']})
